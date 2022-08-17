20f36f0c2922764be7228e6aba1cfe10875b9172
82109 <min22222>
2022-08-17 16:09:24 +0900

    cafe

---
+++ app/src/main/java/com/example/sw_02/CustomAdapter_f.java
@@ -0,0 +1,65 @@
+package com.example.sw_02;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+
+
+public abstract class CustomAdapter_f extends RecyclerView.Adapter<CustomAdapter_f.CustomViewHolder> {
+
+    private ArrayList<f_rst> arrayList;
+    private Context context;
+
+    public CustomAdapter_f(ArrayList<f_rst> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ViewGroup list_cafe;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout. list_cafe, parent, false);
+        CustomViewHolder holder = new CustomViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+
+        holder.tv_name.setText(arrayList.get(position).getName());
+        holder.tv_dong.setText(arrayList.get(position).getDong());
+        holder.tv_star.setText(arrayList.get(position).getStar());
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return (arrayList != null ? arrayList.size() : 0);
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+        TextView tv_name;
+        TextView tv_dong;
+        TextView tv_star;
+
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.tv_name = itemView.findViewById(R.id.tv_name);
+            this.tv_dong = itemView.findViewById(R.id.tv_dong);
+            this.tv_star = itemView.findViewById(R.id.tv_star);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sw_02/cafefood.java
@@ -0,0 +1,63 @@
+package com.example.sw_02;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class cafefood extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<k_rst> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_koreanfood);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>();
+
+        database = FirebaseDatabase.getInstance();
+
+        databaseReference = database.getReference("k_rst");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                arrayList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    k_rst k_rst = snapshot.getValue(k_rst.class);
+                    arrayList.add(k_rst);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("koreanfood", String.valueOf(databaseError.toException()));
+
+            }
+        });
+
+        adapter = new CustomAdapter(arrayList, this);
+        recyclerView.setAdapter(adapter);
+    }
+}
