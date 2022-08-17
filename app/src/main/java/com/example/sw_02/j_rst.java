f3c456e8cabfa3dca5d27c5b18e0dca9b79206df
20211396 <20211396@sungshin.ac.kr>
2022-08-17 16:39:01 +0900

    업데이트3

---
+++ app/src/main/java/com/example/sw_02/j_rst.java
@@ -0,0 +1,34 @@
+package com.example.sw_02;
+
+public class j_rst {
+    private String name;
+    private String dong;
+    private String star;
+
+    public j_rst(){}
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDong() {
+        return dong;
+    }
+
+    public void setDong(String dong) {
+        this.dong = dong;
+    }
+
+    public String getStar() {
+        return star;
+    }
+
+    public void setStar(String star) {
+        this.star = star;
+    }
+}
