40f66ad3fe156fc68b94c01d21c1e0f054e03892
20211396 <20211396@sungshin.ac.kr>
2022-08-17 13:26:08 +0900

    second

---
+++ app/src/main/java/com/example/sw_02/분식.java
@@ -0,0 +1,38 @@
+package com.example.sw_02;
+
+public class 분식 {
+    private String dong;
+    private String name;
+    private int star;
+
+    public 분식() {
+    }
+
+    public String getDong() {
+        return dong;
+    }
+
+    public void setDong(String dong) {
+        this.dong = dong;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+}
+
+
+
