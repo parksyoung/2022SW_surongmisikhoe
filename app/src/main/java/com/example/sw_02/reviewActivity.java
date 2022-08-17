e8334301f878a7c0cef9960af52d78c4ed5ae1f6
parksyoung <qkrtjdudsy@naver.com>
2022-08-17 19:45:37 +0900

    오타 수정2

--- app/src/main/java/com/example/sw_02/reviewActivity.java
+++ app/src/main/java/com/example/sw_02/reviewActivity.java
@@ -33,30 +33,30 @@ public class reviewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_review);
 
         recyclerView = findViewById(R.id.rv); //아이디 연결
-        //recyclerView = setHasFixedSize(true);
+        recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         arrayList = new ArrayList<>(); //식당 객체를 담을 어레이 리스트 어댑터 쪽으로
 
         database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
 
-        databaseReference = database.getReference("name"); //DB데이블 연결
+        databaseReference = database.getReference("shop"); //DB테이블 연결
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //파이어베이스 데이터베이스 데이터를 받아오는 곳
                 arrayList.clear(); //기존 배열 초기화
-                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
-                    ReviewData reviewData = snapshot1.getValue(ReviewData.class);
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    ReviewData reviewData = snapshot.getValue(ReviewData.class);
                     arrayList.add(reviewData); // 담은 데이터를 배열리스트에 넣고 리사이클러뷰로 보낼 준비
                 }
                 adapter.notifyDataSetChanged(); //리스트 저장 및 새로고침
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
                 //DB를 가져오던 중 에러 발생시
-                //생략
+                Log.e("reviewActivity", String.valueOf(databaseError.toException()));
             }
         });
         adapter = new ReviewAdapter(arrayList,this);
