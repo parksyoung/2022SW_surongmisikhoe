074396e895e2d35be7fb96bc95184db0424676dc
parksyoung <qkrtjdudsy@naver.com>
2022-08-17 21:47:41 +0900

    reviewdata -> shop

---
+++ app/src/main/java/com/example/sw_02/shop.java
@@ -0,0 +1,35 @@
+package com.example.sw_02;
+
+public class shop {
+
+    private String name;
+    private String cate_1;
+    private int rate_num;
+
+    public shop() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCate_1() {
+        return cate_1;
+    }
+
+    public void setCate_1(String cate_1) {
+        this.cate_1 = cate_1;
+    }
+
+    public int getRate_num() {
+        return rate_num;
+    }
+
+    public void setRate_num(int rate_num) {
+        this.rate_num = rate_num;
+    }
+
+}
